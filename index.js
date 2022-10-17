43181715d8fa53191ef0df691b651ab802efb091
Abdul Saboor Sohaib <abdulsaboorsoahib@gmail.com>
2022-10-17 19:30:43 +0430

    Add some changes

--- index.js
+++ index.js
@@ -5,29 +5,34 @@ const ourServices = [
         Image: './images/web.png',
         Service: 'Web Application',
         Description: 'We have experienced developer they can work remotely',
+        button: 'Learn More!',
     },
     {
         Image: './images/desktop.png',
         Service: 'Desktop Application',
         Description: 'We have experienced developer they can work remotely',
+        button: 'Learn More!',
     },
     {
         Image: './images/mobile.png',
         Service: 'Mobile Application',
         Description: 'We have experienced developer they can work remotely',
+        button: 'Learn More!',
     }
 ]
 function serviceDetalis(ourServices){
+    // const serviceTitle = document.createElement('h2');
+    // serviceTitle.innerText('Our Services');
+    // services.appendchild(serviceTitle);
     let services = '';
     ourServices.forEach((task) => {
         services += `
-        <div class="cards">
           <div class="mycards">
           <img src="${task.Image}">
-          <h2>${task.Service}</h2>
-          <p>${task.Description}</p>
+          <h2 class="card-title">${task.Service}</h2>
+          <p class="card-description">${task.Description}</p>
+          <button class="card-btn">${task.button}</button>
           </div>
-        </div>
         `
     }); 
     servicesSection.innerHTML = services;
