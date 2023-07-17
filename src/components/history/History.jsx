67df72778104606c3b16b701d33443f8f6dfb3b9
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-07-17 20:33:15 +0430

    Add these components for demo

---
+++ src/components/history/History.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const History = () => (
+
+  <div className="coantainer" id="history">
+    <h2>History</h2>
+  </div>
+
+);
+
+export default History;
---
+++ src/components/statistics/Statistics.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Statistics = () => (
+  <div className="container" id="statistics">
+    <h2>Statistics</h2>
+  </div>
+);
+
+export default Statistics;
