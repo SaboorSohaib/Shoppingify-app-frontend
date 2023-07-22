e8568d71ce8c86a4d5dbba873a76fdd1eba2b8fd
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-07-22 14:48:06 +0430

    Add Catgory component to item and classes for style

--- src/components/item/Item.jsx
+++ src/components/item/Item.jsx
@@ -1,9 +1,61 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getItemsThunk } from '../../redux/Item/itemSlice';
+import Category from '../Category/Category';
+import HeaderText from '../HeaderText/HeaderText';
+import './item.css';
 
-const Item = () => (
-  <section className="container" id="item">
-    <h2>Item</h2>
-  </section>
-);
+const Item = () => {
+  const dispatch = useDispatch();
+  const status = useSelector((state) => state.status);
+  const { categories } = useSelector((state) => state.categories);
+  const { itemsByCategory } = useSelector((state) => state.items);
+  useEffect(() => {
+    if (categories && categories.length > 0) {
+      categories.forEach((category) => {
+        const categoryId = category.id;
+        if (!itemsByCategory[categoryId]) {
+          dispatch(getItemsThunk(categoryId));
+        }
+      });
+    }
+  }, [categories, itemsByCategory, dispatch]);
+
+  if (status === 'loading') {
+    return <div>Loading...</div>;
+  }
+  if (status === 'failed') {
+    return <div>Failed to load data</div>;
+  }
+
+  return (
+    <section className="Item__conatiner" id="item">
+      <div className="headerText">
+        <HeaderText />
+      </div>
+      <div className="category">
+        <Category />
+      </div>
+      <div className="item">
+        {categories.map((category) => (
+          <div key={category.id} className="single__category">
+            <h2 className="category__name">{category.name}</h2>
+            {itemsByCategory[category.id] && (
+            <div className="item__card">
+              {itemsByCategory[category.id].map((item) => (
+                <div className="item__info" key={item.id}>
+                  <p>{item.name}</p>
+                  <span className="plus__button">+</span>
+                </div>
+              ))}
+            </div>
+            )}
+          </div>
+        ))}
+      </div>
+
+    </section>
+  );
+};
 
 export default Item;
