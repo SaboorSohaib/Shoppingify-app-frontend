c474b86007369b9e768808fd672c323838e528e9
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-08-14 11:37:49 +0430

    Change component folder name in lowercase

--- src/components/item/Item.jsx
+++ src/components/item/Item.jsx
@@ -2,8 +2,8 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { getItemsThunk } from '../../redux/Item/itemSlice';
-import Category from '../Category/Category';
-import HeaderText from '../HeaderText/HeaderText';
+import Category from '../category/Category';
+import HeaderText from '../headertext/HeaderText';
 import './item.css';
 
 const Item = () => {
